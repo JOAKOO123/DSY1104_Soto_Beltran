cad81fc9a13c297b4d8f7d8bd3aa0e47f640f8ac
joakoo123 <beltranlive9@gmail.com>
2025-10-16 16:26:39 -0300

    chore: Limpiar estructura inicial del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import { router } from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
