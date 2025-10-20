0b31371ad262176c36a331d1308c4cc22dc6822d
joakoo123 <beltranlive9@gmail.com>
2025-10-20 17:14:13 -0300

    chore: Eliminar carpeta de demostración para unificar el proyecto

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import Header from './components/root/Header';
+import HomePage from './pages/home';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage /> {/*página de inicio */}
+      {/*ruta pa las paginas*/}
+    </div>
+  );
+}
+
+export default App;
